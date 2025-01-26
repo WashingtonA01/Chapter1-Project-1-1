99fb64671c86fed66686c51e47929d1d798a72f4
user <user@DESKTOP-498P8QH>
2025-01-26 21:46:55 +0900

    Add project files.

---
+++ Chapter1 Project 1-1.cpp
@@ -0,0 +1,29 @@
+// File Name: Chapter1 Project 1-1.cpp
+// Programmer: Arianna Washington
+// Date: 1/25/2025
+// This file contains the 'main' function. Program execution begins and ends there.
+//This project calculates the user's pay.
+
+#include <iostream>
+using namespace std;
+
+int main()
+int hours;
+double payrate;
+double pay;
+pay = hours * payrate;
+
+// Get the number of hours worked.
+cout << "How many hours did you work? ";
+cin >> hours;
+
+// Get hourly pay rate.
+cout << "How much do you get paid per hour?";
+cin >>payrate;
+
+// Calculate Pay.
+pay = hours * rate; 
+
+//Display the pay.
+cout << "You have earned $" << pay << "\n";
+}
